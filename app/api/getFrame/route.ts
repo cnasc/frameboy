b681f0bb179bfb6b530b76b0c501e7a0eb40bac6
Christopher Nascone <chris@nasc.one>
2024-02-03 23:42:51 -0500

    clean up console

--- app/api/getFrame/route.ts
+++ app/api/getFrame/route.ts
@@ -5,7 +5,6 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     const response = await fetch(data.url);
     const text = await response.text();
-    console.log(text);
     return NextResponse.json({ html: text }, { status: 200 });
   } catch (error) {
     console.error('Error fetching frame:', error);
