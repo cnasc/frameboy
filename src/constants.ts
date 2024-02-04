79ec94b1752f26437a413b5549f9ce01f1f8f9fa
Christopher Nascone <chris@nasc.one>
2024-02-03 23:32:22 -0500

    update frame spec url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
 export const APP_NAME = 'f r a m e b o y';
 export const GITHUB_REPO = 'https://github.com/cnasc/frameboy';
-export const FRAME_SPEC_URL =
-  'https://warpcast.notion.site/Farcaster-Frames-4bd47fe97dc74a42a48d3a234636d8c5';
+export const FRAME_SPEC_URL = 'https://docs.farcaster.xyz/reference/frames/spec';
