9dd1c4def55810c75f381858378ebabc711f48d6
Christopher Nascone <chris@nasc.one>
2024-02-04 16:11:55 -0500

    type massaging

--- src/utils/schemaValidation.ts
+++ src/utils/schemaValidation.ts
@@ -13,9 +13,11 @@ export const vNextSchema = yup.object({
         .optional()
         .test(
           `has-button-${index - 1}`,
-          'Button index values must be in sequence, starting at 1',
+          `Button index values must be in sequence, starting at 1. Failed on index: ${2}`,
           (value, ctx) => {
             if (value) {
+              // we only care whether or not `prevButton` is undefined
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
               const prevButton = ctx.parent[`fc:frame:button:${index - 1}`];
               return !!prevButton;
             }
@@ -31,7 +33,10 @@ export const vNextSchema = yup.object({
       [`fc:frame:button:${index}:action`]: yup
         .string()
         .optional()
-        .matches(/(post$)|post_redirect$/, 'button action must be "post" or "post_url"'),
+        .matches(
+          /(post$)|post_redirect$/,
+          `button action must be "post" or "post_url". Failed on index: ${index}`,
+        ),
     }),
     {},
   ),
@@ -39,14 +44,19 @@ export const vNextSchema = yup.object({
     .string()
     .optional()
     .test('url-has-valid-size', 'post_url has maximum size of 256 bytes', (value) => {
-      return new Blob([value]).size <= 256;
+      // test only fires when `value` is defined
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return new Blob([value!]).size <= 256;
     }),
   'fc:frame:input:text': yup
     .string()
     .optional()
     .test('url-has-valid-size', 'input:text has maximum size of 32 bytes', (value) => {
-      return new Blob([value]).size <= 32;
+      // test only fires when `value` is defined
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return new Blob([value!]).size <= 32;
     }),
 });
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/consistent-type-definitions
 export interface FrameVNext extends yup.InferType<typeof vNextSchema> {}
