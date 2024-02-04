dced8bceb6d9a0f705fe7da513d989bdd5e1b19d
Christopher Nascone <chris@nasc.one>
2024-02-03 20:00:58 -0500

    chore: we'll use onchainkit separately

--- onchainKit/core/balance.ts
+++
@@ -1,8 +0,0 @@
-import type { WagmiFetchBalanceResult } from '../types';
-
-/**
- * TODO Docs
- */
-export const getAccountBalance = (data?: WagmiFetchBalanceResult) => {
-  return `${data?.formatted.slice(0, 5)} ${data?.symbol}`;
-};
